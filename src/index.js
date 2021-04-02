4cc2e29cf9e7d2ba21319d777bd56800ff66114e
Tanvir Hossen Bappy <tanvir15-7706@diu.edu.bd>
2021-04-02 11:42:32 +0600

    jsx clock element

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import ReactDOM from 'react-dom';
+
+setInterval(()=>
+{
+    const element = (
+        <h1 className="heading">
+        <span className="text">
+            The time is {new Date().toLocaleTimeString()}
+        </span>
+        </h1>
+    );
+    ReactDOM.render(element,document.getElementById('root'))
+},100);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
